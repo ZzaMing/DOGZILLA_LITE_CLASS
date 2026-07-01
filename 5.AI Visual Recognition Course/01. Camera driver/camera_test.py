ffb889297b0c378029b75fc7759e7374041eccab
zzaming <whaudgus123@naver.com>
2026-07-01 15:14:10 +0900

    300회 임시모델

--- 5.AI Visual Recognition Course/01. Camera driver/camera_test.py
+++ 5.AI Visual Recognition Course/01. Camera driver/camera_test.py
@@ -1,26 +1,3 @@
-# import cv2
-# from picamera2 import Picamera2
-
-# try:
-#   # For webcam input:
-#   picam2 = Picamera2()
-#   picam2.configure(
-#       picam2.create_preview_configuration(main={"format": "RGB888", "size": (320, 240)})
-#   )
-#   picam2.start()
-
-#   while True:
-#     frame = picam2.capture_array() 
-#     frame = cv2.flip(frame, 1)
-    
-#     cv2.imshow("image1",frame)
-
-#     if cv2.waitKey(5) & 0xFF == 27:
-#       break
-# except:
-#   picam2.stop()
-#   picam2.close()
-
 import cv2
 import os
 from ultralytics import YOLO
@@ -28,7 +5,7 @@ from ultralytics import YOLO
 # 현재 실행 중인 스크립트 파일(.py)의 절대 경로 폴더 알아내기
 current_dir = os.path.dirname(os.path.abspath(__file__))
 # 현재 폴더 경로에 "best.pt" 파일명 결합
-model_path = os.path.join(current_dir, "recycle_person.pt")
+model_path = os.path.join(current_dir, "rec_300.pt")
 
 # 동적으로 생성한 절대 경로를 사용해 모델 로드
 model = YOLO(model_path)
@@ -50,7 +27,14 @@ try:
         
         # 2. YOLO11 모델에게 프레임을 던져주고 인식 결과(박스, 확률)를 받아오기
         # conf=0.5 옵션: AI가 50% 이상 확신하는 객체만 화면에 표시
-        results = model.predict(source=frame, show=False, conf=0.75)
+        results = model.predict(
+            source=frame, 
+            show=False, 
+            conf=0.36,
+            device='cpu',      # 1. CPU 연산 강제 지시
+            imgsz=320,         # 2. 학습 모델과 동일한 해상도로 연산량 최소화
+            verbose=False      # 3. 터미널 I/O 병목 방지를 위한 로그 출력 끄기
+        )
         
         # 3. 인식 결과(테두리 박스와 이름)가 그려진 이미지 데이터 추출
         annotated_frame = results[0].plot()
